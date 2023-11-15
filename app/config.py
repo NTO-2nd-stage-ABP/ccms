0d0bbb7799d44fe20e9c6dddb1b9a84e86075fb4
zobweyt <zobweyt@gmail.com>
2023-11-15 23:22:48 +0300

    Add `DATABASE_ECHO` configuration

--- app/config.py
+++ app/config.py
@@ -4,3 +4,4 @@ from decouple import config
 
 APP_NAME: Final[str] = config("APP_NAME")
 DATABASE_URL: Final[str] = config("DATABASE_URL", default="sqlite:///db.sqlite3")
+DATABASE_ECHO: Final[bool] = config("DATABASE_ECHO", default=False, cast=bool)
--- app/db/__init__.py
+++ app/db/__init__.py
@@ -3,6 +3,6 @@ from typing import Final
 from sqlmodel import create_engine
 from sqlalchemy.future.engine import Engine
 
-from app.config import DATABASE_URL
+from app.config import DATABASE_URL, DATABASE_ECHO
 
-ENGINE: Final[Engine] = create_engine(DATABASE_URL, echo=True)
+ENGINE: Final[Engine] = create_engine(DATABASE_URL, echo=DATABASE_ECHO)
