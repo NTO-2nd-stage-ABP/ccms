b4b8eef57dc7881e4cddda23f00862c114004dfe
zobweyt <zobweyt@gmail.com>
2023-11-16 19:49:51 +0300

    Update models to fit the task

--- app/db/models.py
+++ app/db/models.py
@@ -1,19 +1,46 @@
-from typing import Optional
+from enum import Enum
+from typing import Optional, List
 from datetime import datetime
 
 from sqlmodel import SQLModel, Field, Relationship
 
 
-class Base(SQLModel):
-    id: Optional[int] = Field(primary_key=True)
+class BaseModel(SQLModel):
+    """
+    Represents the base for defining models.
+    """
+
+    id: Optional[int] = Field(default=None, primary_key=True)
 
 
-class Space(Base, table=True):
+class Section(Enum):
+    """
+    Represents a section to categorize events.
+    """
+
+    EDUCATION = 1
+    ENLIGHTMENT = 2
+    ENTERTAINMENT = 3
+
+
+class Type(BaseModel, table=True):
+    """
+    Represents the type of an event.
+    """
+
     name: str
+    section: Section
+    
+    events: List["Event"] = Relationship(back_populates="type")
 
 
-class Event(Base, table=True):
-    name: str
+class Event(BaseModel, table=True):
+    """
+    Represents an event registered on a specific date.
+    """
+
     date: datetime
-    space_id: int = Field(default=None, foreign_key="space.id")
-    space: Space = Relationship(back_populates="space")
+    description: Optional[str] = None
+
+    type_id: Optional[int] = Field(default=None, foreign_key="type.id")
+    type: Type = Relationship(back_populates="events")
