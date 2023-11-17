40a07d43d4bd48a7479d9f0a55359892e8aa3ee6
zobweyt <zobweyt@gmail.com>
2023-11-17 23:12:33 +0300

    Update UI

--- app/ui/main_window.py
+++ app/ui/main_window.py
@@ -15,7 +15,11 @@ class MainWindow(QMainWindow):
         uic.loadUi("app/ui/main_window.ui", self)
 
         self.statusbar.showMessage("Выберите элемент")
-        self.events_type_manager_action.triggered.connect(self.openEventsTypeManager)
+        self.create_action.triggered.connect(self.onCreateActionTriggered)
+        self.events_type_manager_action.triggered.connect(self.onEventsTypeManagerActionTriggered)
 
-    def openEventsTypeManager(self):
+    def onEventsTypeManagerActionTriggered(self):
         uic.loadUi("app/ui/dialogs/events_type_manager.ui").exec()
+
+    def onCreateActionTriggered(self):
+        uic.loadUi("app/ui/dialogs/event_edit.ui").exec()
