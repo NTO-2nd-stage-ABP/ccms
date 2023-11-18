b14d740b7d369d59c70619eaa62257d95ab9291c
zobweyt <zobweyt@gmail.com>
2023-11-18 15:12:54 +0300

    refactoring

--- app/dialogs.py
+++ app/dialogs.py
@@ -1,7 +1,7 @@
 from PyQt6 import uic
-from PyQt6.QtWidgets import QDialog, QMessageBox
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QStandardItemModel, QStandardItem
+from PyQt6.QtWidgets import QDialog, QMessageBox
 
 from sqlmodel import Session, select
 
@@ -14,79 +14,79 @@ class TypeManagerDialog(QDialog):
         super().__init__()
         uic.loadUi("app/ui/dialogs/events_type_manager.ui", self)
 
-        self.eventTypeIds = {}
         self.model = QStandardItemModel()
+        self.eventTypeIds = dict()
+        self.refreshEventTypes()
+
+        self.model.dataChanged.connect(self.onDataChanged)
+        self.addButton.clicked.connect(self.onAddButtonClicked)
+        self.delButton.clicked.connect(self.onDelButtonClicked)
+
+    def refreshEventTypes(self):
         self.listView.setModel(self.model)
 
         with Session(ENGINE) as session:
             eventTypes = session.exec(select(EventType)).all()
 
         for eventType in eventTypes:
-            self.eventTypeIds[self.model.rowCount()] = eventType.id
             self.addEventTypeToListView(eventType)
-
-        self.model.dataChanged.connect(self.onDataChanged)
-        self.addButton.clicked.connect(self.onAddButtonClicked)
-        self.delButton.clicked.connect(self.onDelButtonClicked)
 
     def onAddButtonClicked(self):
         with Session(ENGINE) as session:
-            latestEventType = session.exec(
-                select(EventType).order_by(EventType.id.desc()).limit(1)
-            ).first()
-            newEventType = EventType(name=f"Вид {1 if latestEventType == None else latestEventType.id + 1}")
-            self.addEventTypeToListView(newEventType)
+            latestEventType = (
+                session.query(EventType)
+                .order_by(EventType.id.desc())
+                .limit(1)
+                .one_or_none()
+            )
+            newEventType = EventType(
+                name=f"Вид {latestEventType.id + 1 if latestEventType else 1}"
+            )
             session.add(newEventType)
             session.commit()
-            self.eventTypeIds[self.model.rowCount() - 1] = newEventType.id
+            self.addEventTypeToListView(newEventType)
 
     def onDelButtonClicked(self):
         currentRowIndex = self.listView.currentIndex().row()
-        
+
         with Session(ENGINE) as session:
-            print(self.eventTypeIds.keys())
-            print("pk", self.eventTypeIds[currentRowIndex])
             eventType = session.get(EventType, self.eventTypeIds[currentRowIndex])
             session.delete(eventType)
             session.commit()
 
         self.model.removeRow(currentRowIndex)
-        print(self.eventTypeIds.keys())
-        self.eventTypeIds.pop(currentRowIndex)
-        print(self.eventTypeIds.keys())
-        values = self.eventTypeIds.values()
-        self.eventTypeIds = {}
-        for i, value in enumerate(values):
-            print(i)
-            self.eventTypeIds[i] = value
-        print(self.eventTypeIds.keys())
+
+        del self.eventTypeIds[currentRowIndex]
+        self.eventTypeIds.update(
+            {i: value for i, value in enumerate(self.eventTypeIds.values())}
+        )
 
     def onDataChanged(self, index):
         with Session(ENGINE) as session:
-            print(self.eventTypeIds.keys())
-            newName = index.data(Qt.ItemDataRole.DisplayRole)
+            name = index.data(Qt.ItemDataRole.DisplayRole)
             eventType = session.get(EventType, self.eventTypeIds[index.row()])
 
-            if session.query(
-                select(EventType).where(EventType.name == newName).exists()
-            ).scalar():
-                QMessageBox.critical(
-                    self,
-                    "Вид мероприятия с таким названием уже существует!",
-                    "Введите другое название.",
-                )
-                self.model.blockSignals(True)
-                self.model.setData(index, eventType.name, Qt.ItemDataRole.DisplayRole)
-                self.model.blockSignals(False)
+            if session.query(select(EventType).filter_by(name=name).exists()).scalar():
+                self.showUniqueNameWarning()
+                self.setModelData(index, eventType.name, Qt.ItemDataRole.DisplayRole)
                 return
 
-            eventType.name = newName
+            eventType.name = name
             session.add(eventType)
             session.commit()
+
+    def setModelData(self, index, value, role):
+        self.model.blockSignals(True)
+        self.model.setData(index, value, role)
+        self.model.blockSignals(False)
+
+    def showUniqueNameWarning(self):
+        QMessageBox.warning(self, "Предупреждение", "Такой вид уже существует!")
 
     def addEventTypeToListView(self, eventType: EventType):
         item = QStandardItem(eventType.name)
         item.setFlags(item.flags() | Qt.ItemFlag.ItemIsEditable)
+        self.eventTypeIds[self.model.rowCount()] = eventType.id
         self.model.appendRow(item)
 
 
